0c6946363fbf0ec0f63880628a87c2297a3e6414
k8andrews <kathleenanneandrews@gmail.com>
2017-09-18 10:47:59 -0400

    Added ComicBook Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,12 +10,14 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-              
-            }
-            return Content("Hello from the Comic Books Controller.");
+            /* if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+             {
+                 return Redirect("/");
+
+             }
+             return Content("Hello from the Comic Books Controller.");*/
+
+            return View();
                     
         }
     }
